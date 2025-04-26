27e607f989965a2d85efdc0361c877d7eb40e29d
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-04-26 10:37:24 +0200

    Improved and fixed typing in margin_app GetAllMediator

--- margin/margin_app/app/api/common.py
+++ margin/margin_app/app/api/common.py
@@ -2,10 +2,23 @@
 Common API for get all endpoints.
 """
 
-from typing import Any, Optional
+from typing import Any, Optional, Dict, Protocol
 import logging
 from fastapi import HTTPException, status
-from typing import Dict
+
+
+class PaginationCrudI(Protocol):
+    """
+    Interface for crud_object to require support of used pagination methods
+    """
+
+    async def get_objects(self, limit: Optional[int], offset: Optional[int]) -> Any:
+        """Fetch objects with supplied limit and offset"""
+        ...
+
+    async def get_objects_amounts(self) -> int:
+        """Fetch total amount of objects"""
+        ...
 
 
 class GetAllMediator:
@@ -13,7 +26,9 @@ class GetAllMediator:
     Mediator for handling "get all" requests.
     """
 
-    def __init__(self, crud_object: Any, limit: Optional[int], offset: int):
+    def __init__(
+        self, crud_object: PaginationCrudI, limit: Optional[int], offset: Optional[int]
+    ):
         self.crud_object = crud_object
         self.limit = limit
         self.offset = offset
