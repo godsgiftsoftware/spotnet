ee3c0161bc7a2cfe7909bd642739679764bbf064
kimcascante <kiimcm9@gmail.com>
2025-05-30 18:20:23 -0600

    feat(admin-dashboard): add AccountValuesCard component to match design example

--- margin/margin_frontend/src/ui/admin/dashboard/dashboardMain.tsx
+++ margin/margin_frontend/src/ui/admin/dashboard/dashboardMain.tsx
@@ -1,6 +1,7 @@
 import Assets from './assets';
 import AdminLayout from '../AdminLayout';
 import DashboardStats from './DashboardStats';
+import AccountValuesCard from './AccountValuesCard';
 
 const AdminDashboard: React.FC = () => {
   const mockAssetsData = {
@@ -11,7 +12,8 @@ const AdminDashboard: React.FC = () => {
   };
   return (
     <AdminLayout>
-      <div className='text-white'>
+      <div className='text-white flex flex-col items-center gap-8'>
+        <AccountValuesCard />
         <DashboardStats />
         <Assets className='w-full max-w-[900px]' data={mockAssetsData} />
       </div>
