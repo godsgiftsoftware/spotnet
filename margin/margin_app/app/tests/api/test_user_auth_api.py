50a23962af5768daa2b4e16789b592dbe9c62ac7
Sani Abubakar Adam <Athernee@gmail.com>
2025-06-02 15:57:17 +0100

    test: fix user login test

--- margin/margin_app/app/tests/api/test_user_auth_api.py
+++ margin/margin_app/app/tests/api/test_user_auth_api.py
@@ -7,6 +7,7 @@ import pytest
 from datetime import timedelta
 from types import SimpleNamespace
 from unittest.mock import AsyncMock, patch
+from fastapi.testclient import TestClient
 
 from fastapi import status
 
@@ -15,6 +16,7 @@ from app.crud.user import user_crud
 from app.services.auth.security import verify_password
 from app.services.auth.base import create_access_token, create_refresh_token
 
+client = TestClient(app)
 
 LOGIN_URL = "/api/auth/login"
 
@@ -49,13 +51,18 @@ def patch_tokens():
     Patch create_access_token, create_refresh_token to return fixed strings,
     - easy to assert on the response JSON and cookie
     """
-    with patch("app.services.auth.base.create_access_token", new_callable=AsyncMock) as mock_access:
-        with patch("app.services.auth.base.create_refresh_token", new_callable=AsyncMock) as mock_refresh:
+    with patch(
+        "app.services.auth.base.create_access_token",
+        new_callable=AsyncMock) as mock_access:
+        with patch(
+            "app.services.auth.base.create_refresh_token", new_callable=AsyncMock) as mock_refresh:
             mock_access.return_value = "fixed-access-token"
             mock_refresh.return_value = "fixed-refresh-token"
             yield (mock_access, mock_refresh)
 
-def test_login_success_sets_cookie_and_returns_access_token(patch_get_user, patch_verify_pwd, patch_tokens):
+def test_login_success_sets_cookie_and_returns_access_token(
+    patch_get_user, patch_verify_pwd, patch_tokens):
+    """ Test successful user login"""
     payload = {"email": test_user.email, "password": "correct-password"}
 
     response = client.post(LOGIN_URL, json=payload)
@@ -85,6 +92,7 @@ def test_login_success_sets_cookie_and_returns_access_token(patch_get_user, patc
     )
 
 def test_login_user_not_found_returns_404(patch_get_user):
+    """ Test user when user not found"""
     patch_get_user.return_value = None
     payload = {"email": "no_user@test.com", "password": "whatever"}
 
@@ -95,6 +103,7 @@ def test_login_user_not_found_returns_404(patch_get_user):
     patch_get_user.assert_awaited_once_with("email", "nouser@test.com")
 
 def test_login_invalid_password_returns_401(patch_verify_pwd):
+    """ Test user login with invalid password"""
     patch_verify_pwd.return_value = False
     payload = {"email": test_user.email, "password": "wrong-password"}
 
