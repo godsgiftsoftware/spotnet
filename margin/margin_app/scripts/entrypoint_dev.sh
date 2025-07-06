739e636650420c5c3482d8451a3a9ee67324af66
Ofonime Nsikak <141313914+Dannynsikak@users.noreply.github.com>
2025-07-06 13:20:24 +0100

    add: add notification bot to compose (#921)
    * add: add notification bot to compose
    
    * fix: resolved the failed build
    
    * fix: resolved the failed build

--- margin/margin_app/scripts/entrypoint_dev.sh
+++ margin/margin_app/scripts/entrypoint_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Activate virtual environment"
-source $(poetry env info --path)/bin/activate
+
 
 echo "Run migration with alembic"
 poetry run alembic -c /app/alembic.ini upgrade head
